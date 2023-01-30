d43fa5733547efb463eb9ffdd045217e08088b78
jeraxProg <113060248+jeraxProg@users.noreply.github.com>
2023-01-30 15:22:47 +0800

    hascorp

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = process.env.NODE_ENV !== 'production' ? "http://localhost:3000" : 'https://hascorpjob.vercel.app/' ;
+const baseUrl = process.env.NODE_ENV !== 'production' ? "http://localhost:3000" : 'https://hasjobcor.vercel.app/' ;
 
 module.exports = baseUrl;
